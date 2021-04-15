50d08c3d340fb72575feaf517de863f86b73133c
Vishal Singla <singlavishal121@gmail.com>
2021-04-15 12:51:18 +0530

    C program added

---
+++ Participants/participants.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+   char z[100] = "Current Collaborators on this project \n 1. Vishal Singla\n 2. Suryansh Chopra.";
+
+   printf("%s", z); // %s is format specifier
+
+   return 0;
+}
